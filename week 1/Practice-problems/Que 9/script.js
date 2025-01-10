2570282891a444660e94373ca7580a06e8d210c5
Mahesh Patil <maheshpatilmp937@gmail.com>
2025-01-10 16:03:24 +0530

    Question no.9

---
+++ week 1/Practice-problems/Que 9/script.js
@@ -0,0 +1,7 @@
+var textarea = document.querySelector("textarea");
+var counter = document.querySelector("#counter");
+ 
+
+textarea.addEventListener("input", function(){
+   counter.textContent = textarea.value.length;
+})
