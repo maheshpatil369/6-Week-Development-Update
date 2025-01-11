83b57b7527b8b51dafa86b1f64924ee37e735c1e
Mahesh Patil <maheshpatilmp937@gmail.com>
2025-01-11 15:51:29 +0530

    Update animation

--- week 2/Project 1/script.js
+++ week 2/Project 1/script.js
@@ -7,6 +7,13 @@ tl.from("#nav h3",{
   duration:0.8,
   stagger:0.3
 })
+tl.from("#nav2 h4",{
+    y: 50,
+    opacity:0,
+    delay:0.4,
+    duration:0.8,
+    stagger:0.3
+  })
 
 tl.from("#main h1",{
     x:-500,
